fdc53bd4632dfda3e2fb1c7a10b029ff19ae9c08
Adrian Pauli <apauli@puzzle.ch>
2018-06-07 17:18:04 +0200

    Try to use rpchost

--- lib/terracoin_config.py
+++ lib/terracoin_config.py
@@ -28,7 +28,7 @@ class TerracoinConfig():
     @classmethod
     def get_rpc_creds(self, data, network='mainnet'):
         # get rpc info from terracoin.conf
-        match = re.findall(r'rpc(user|password|port)=(.*?)$', data, re.MULTILINE)
+        match = re.findall(r'rpc(user|password|port|host)=(.*?)$', data, re.MULTILINE)
 
         # python >= 2.7
         creds = {key: value for (key, value) in match}
